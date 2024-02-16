68e5caf766f6a0fe5da6fb0545c7e99334ca2e7f
chowcli <136085102+chowcli@users.noreply.github.com>
2024-02-16 09:22:22 +0700

    Add delete, find, levelOrder method

--- BST.js
+++ BST.js
@@ -20,22 +20,15 @@ class BST {
 
   buildTree(array) {
     const sortArr = mergeSort(array);
-    const rootNode = this.#sortedArrToBST(sortArr);
-
-    return rootNode;
+    return this.#sortedArrToBST(sortArr);
   }
 
   #insertRecursion(root, value) {
-    if (root === null) {
-      root = Node(value);
-      return root;
-    }
+    if (root === null) return Node(value);
 
     if (value < root.value) {
       root.left = this.#insertRecursion(root.left, value);
-    }
-
-    if (value > root.value) {
+    } else if (value > root.value) {
       root.right = this.#insertRecursion(root.right, value);
     }
 
@@ -46,7 +39,118 @@ class BST {
     this.root = this.#insertRecursion(this.root, value);
   }
 
-  delete(value) {}
+  #minValueNode(node) {
+    let current = node;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current;
+  }
+
+  #deleteNode(root, value) {
+    if (root === null) return root;
+
+    if (value < root.value) {
+      root.left = this.#deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.#deleteNode(root.right, value);
+    } else {
+      // Case 1: Node to delete has no children or 1 child
+      if (root.left === null) return root.right;
+      else if (root.right === null) return root.left;
+
+      // Case 2: Node to delete has 2 children
+
+      // 1st method
+      // /*
+
+      let temp = this.#minValueNode(root.right);
+      root.value = temp.value;
+      root.right = this.#deleteNode(root.right, temp.value);
+
+      // */
+
+      // 2nd method
+      /* 
+      
+      let parent = root;
+      let successor = root.right;
+
+      while (successor.left !== null) {
+        parent = successor;
+        successor = successor.left;
+      }
+
+      root.value = successor.value;
+
+      // successor.left is null, doesn't enter the loop case
+      if (parent === root) {
+        parent.right = successor.right;
+      } else {
+        // enter the loop case
+        parent.left = successor.right;
+      }
+
+      */
+    }
+
+    return root;
+  }
+
+  delete(value) {
+    this.root = this.#deleteNode(this.root, value);
+  }
+
+  #findRecursion(root, value) {
+    if (root === null || root.value === value) return root;
+
+    if (value < root.value) {
+      return this.#findRecursion(root.left, value);
+    } else if (value > root.value) {
+      return this.#findRecursion(root.right, value);
+    }
+  }
+
+  find(value) {
+    // Recursive method
+    // return this.#findRecursion(this.root, value);
+
+    // Iterative method
+    let temp = this.root;
+
+    while (temp !== null) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return temp;
+      }
+    }
+
+    return null;
+  }
+
+  levelOrder(cb = null) {
+    let queue = [this.root];
+    let temp = this.root;
+    const array = [];
+
+    while (queue.length > 0) {
+      if (cb) {
+        cb(temp);
+      }
+
+      if (temp.left !== null) queue.push(temp.left);
+      if (temp.right !== null) queue.push(temp.right);
+
+      array.push(queue.shift().value);
+
+      temp = queue[0];
+    }
+
+    return array;
+  }
 }
 
 module.exports = BST;
--- test.js
+++ test.js
@@ -10,3 +10,23 @@ tree.insert(20);
 tree.insert(15);
 
 prettyPrint(tree.root);
+console.log("\n");
+
+tree.delete(9);
+tree.delete(6345);
+tree.delete(0);
+tree.delete(10);
+tree.delete(8);
+prettyPrint(tree.root);
+console.log("\n");
+
+console.log(tree.find(324));
+console.log(tree.find(0));
+console.log(tree.find(3));
+console.log(tree.find(9));
+console.log("\n");
+
+const cb = node => {
+  console.log(node.value);
+};
+console.log(tree.levelOrder(cb));
