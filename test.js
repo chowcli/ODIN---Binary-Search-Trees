7489170d4c9f121742a3afe66dd76cf07778627b
chowcli <136085102+chowcli@users.noreply.github.com>
2024-02-20 20:53:34 +0700

    Finish driver script to test BST

--- test.js
+++ test.js
@@ -1,32 +1,41 @@
 const BST = require("./BST");
 const { prettyPrint } = require("./supportFnc");
 
-const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const tree = new BST(array);
-tree.insert(10);
-tree.insert(0);
-tree.insert(29);
-tree.insert(20);
-tree.insert(15);
+function randomNumArr() {
+  const arr = [];
 
+  for (let i = 0; i < 15; i++) {
+    arr.push(Math.floor(Math.random() * 100));
+  }
+
+  return arr;
+}
+
+function randomInsert(bst) {
+  for (let i = 0; i < 10; i++) {
+    bst.insert(Math.floor(Math.random() * (1000 - 101 + 1)) + 101);
+  }
+}
+
+const tree = new BST(randomNumArr());
 prettyPrint(tree.root);
-console.log("\n");
+console.log("Build new BST");
+console.log("Tree balance:", tree.isBalanced());
+console.log("LevelOrder =>", tree.levelOrder());
+console.log("PreOrder =>", tree.preOrder());
+console.log("InOrder =>", tree.inOrder());
+console.log("PostOrder =>", tree.postOrder(), "\n");
 
-tree.delete(9);
-tree.delete(6345);
-tree.delete(0);
-tree.delete(10);
-tree.delete(8);
+randomInsert(tree);
 prettyPrint(tree.root);
-console.log("\n");
+console.log("After insert new value");
+console.log("Tree balance:", tree.isBalanced(), "\n");
 
-console.log(tree.find(324));
-console.log(tree.find(0));
-console.log(tree.find(3));
-console.log(tree.find(9));
-console.log("\n");
-
-const cb = node => {
-  console.log(node.value);
-};
-console.log(tree.levelOrder(cb));
+tree.rebalance();
+console.log("After rebalance tree");
+console.log("Tree balance:", tree.isBalanced());
+console.log("LevelOrder =>", tree.levelOrder());
+console.log("PreOrder =>", tree.preOrder());
+console.log("InOrder =>", tree.inOrder());
+console.log("PostOrder =>", tree.postOrder(), "\n");
+prettyPrint(tree.root);
